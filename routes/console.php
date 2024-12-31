adcfea00ff43a0a39c02e1f566ef360809910445
maxwellmezadre <maxwellmezadre@gmail.com>
2024-12-31 18:33:14 -0300

    chore(deps): add larastan and fixed codebase

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,14 @@
 
 declare(strict_types=1);
 
+use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function (): void {
+
+    /* @var Command $this */
+    /* @phpstan-ignore-next-line */
     $this->comment(Inspiring::quote());
+
 })->purpose('Display an inspiring quote')->hourly();
